bc6cad053fd466b3222c91cc9cd8d711dc10f43a
Dawid-Dahl <dawid.dahl@kreationsbyran.se>
2022-01-19 16:57:38 +0100

    second commit

---
+++ client/src/components/TextDropdownField.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import fieldHolder from "components/FieldHolder/FieldHolder";
+import {
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Input,
+  InputGroup,
+  InputGroupButtonDropdown,
+} from "reactstrap";
+
+const TextDropdownField = (props) => {
+  const { textField, dropdownField, onAutofill } = props;
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [content, setContent] = useState(textField.value);
+  const [dropdown, setDropdown] = useState(dropdownField.value);
+  const dropdownOptions = dropdownField.source;
+
+  useEffect(() => {
+    setContent(textField.value);
+  }, [textField.value]);
+
+  useEffect(() => {
+    setDropdown(dropdownField.value);
+  }, [dropdownField.value]);
+
+  useEffect(() => {
+    if (typeof onAutofill !== "function") {
+      return;
+    }
+
+    onAutofill(textField.name, content);
+  }, [content]);
+
+  useEffect(() => {
+    if (typeof onAutofill !== "function") {
+      return;
+    }
+
+    onAutofill(dropdownField.name, dropdown);
+  }, [dropdown]);
+
+  const selectedItem = dropdownOptions.find(
+    (option) => option.value == dropdown
+  );
+  // This handles the case where the selected item isn't
+  // passed through as the source item as it's been removed
+  const selectedTitle = selectedItem ? selectedItem.title : dropdown;
+
+  return (
+    <InputGroup>
+      <Input
+        name={textField.name}
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <InputGroupButtonDropdown
+        name={dropdownField.name}
+        addonType="append"
+        isOpen={dropdownOpen}
+        toggle={() => setDropdownOpen(!dropdownOpen)}
+      >
+        <DropdownToggle caret outline>
+          {selectedTitle}
+        </DropdownToggle>
+        <DropdownMenu>
+          {dropdownOptions.map((option) => (
+            <DropdownItem
+              dangerouslySetInnerHTML={{ __html: option.title }}
+              value={option.value}
+              disabled={option.disabled}
+              onClick={() => setDropdown(option.value)}
+            />
+          ))}
+        </DropdownMenu>
+      </InputGroupButtonDropdown>
+    </InputGroup>
+  );
+};
+
+export default fieldHolder(TextDropdownField);

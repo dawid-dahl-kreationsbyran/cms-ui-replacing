0df18ce98454068d75ac43a04d701ba6b5af0234
Dawid-Dahl <dawid.dahl@kreationsbyran.se>
2022-01-19 19:21:49 +0100

    new commit

--- client/src/boot/registerComponents.js
+++ client/src/boot/registerComponents.js
@@ -1,8 +1,14 @@
 import Injector from "lib/Injector";
 import TextDropdownField from "../components/TextDropdownField";
+import CharacterCounter from "../components/CharacterCounter";
 
 export default () => {
   Injector.component.registerMany({
     TextDropdownField,
+    CharacterCounter,
   });
 };
+
+Injector.transform("character-count-transform", (updater) => {
+  updater.component("TextField", CharacterCounter);
+});

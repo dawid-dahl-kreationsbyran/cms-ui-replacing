3e04711d6781bb929389cfcde04d2000e9a79cf4
Dawid-Dahl <dawid.dahl@kreationsbyran.se>
2022-01-20 13:03:28 +0100

    new commit

--- client/src/boot/registerComponents.js
+++ client/src/boot/registerComponents.js
@@ -5,10 +5,7 @@ import CharacterCounter from "../components/CharacterCounter";
 export default () => {
   Injector.component.registerMany({
     TextDropdownField,
-    CharacterCounter,
   });
-
-  console.log("HEY");
 
   Injector.transform("character-count-transform", (updater) => {
     updater.component("TextField", CharacterCounter);
